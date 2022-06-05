68cc5253dd81bb5eee1938130cdccd2765119dba
hyechan <dhgpcks123@naver.com>
2022-06-05 21:12:43 +0900

    기본 라이브러리추가: axios, vue-router, vuex

---
+++ src/pages/error/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>page not found</h1>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style></style>
---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import { createStore } from 'vuex'
+import modules from './modules/modules.js'
+
+export default createStore({
+  modules,
+})
